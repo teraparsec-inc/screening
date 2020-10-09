66a3dba91e64651a3c7273bf45492c375ff9de4e
Anand <anandc823@gmail.com>
2020-10-09 17:55:48 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import DragObject from "./DragObject"
+var windowHeight = Dimensions.get('window').height
+
 
 export default function App() {
+
+  //This state variable keeps track of the current color, which should change on magnetize!
+  const [dropColor,updateDropColor] = useState("blue")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <DragObject intialLocation={{top:50,left:100}}/>
+
+      <View style={styles.lockContainer}>
+        <Text>Drag Here To Magnetize!</Text>
+        <View style={[styles.lockDrop,{borderColor:dropColor}]} />
+      </View>
+
     </View>
   );
 }
@@ -16,6 +29,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center"
   },
+  lockContainer: {
+    position: "absolute",
+    top: windowHeight * .7
+  },
+  lockDrop: {
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+    alignSelf: "center",
+    borderWidth: 1,
+    margin: 5,
+  }
 });
---
+++ DragObject.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+var windowHeight = Dimensions.get('window').height
+
+
+export default function DragObject(props) {
+
+    return(
+        //Boilerplate style: Implement your draggable component here!
+        <View style={[styles.bubble]}>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    bubble:{
+        position:"absolute",
+        width:70,
+        height:70,
+        borderRadius:35,
+        backgroundColor:"green",
+    }
+})
