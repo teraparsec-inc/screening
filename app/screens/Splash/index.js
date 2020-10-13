0bd94fb516fc9b78ec18481ffc7650c2ec1836b9
shivam <48347219+enggmspl@users.noreply.github.com>
2020-10-13 20:49:50 +0530

    set space

--- app/screens/Splash/index.js
+++ app/screens/Splash/index.js
@@ -18,7 +18,6 @@ export default function Splash() {
         <Text>Drag Here To Magnetizess!</Text>
         <View style={[styles.lockDrop,{borderColor:dropColor}]} />
       </View>
-
     </View>
   );
 }
