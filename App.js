58afbe614b2f10a7cdc2228d598c6d72a6f1cc9e
Alexey Bukhmiller <alex.bukhmiller@sharp-dev.net>
2020-10-13 12:19:23 +0300

    test task
    Finished test task

--- App.js
+++ App.js
@@ -1,39 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import DragObject from "./DragObject"
-var windowHeight = Dimensions.get('window').height
+import React, { useState, Component } from "react";
+import { StyleSheet, Text, View, Dimensions, SafeAreaView } from "react-native";
+import DragObject from "./DragObject";
 
+var windowHeight = Dimensions.get("window").height;
 
-export default function App() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropCircle: { x: 0, y: 0, radius: 0 },
+      itemCircle: { x: 0, y: 0, radius: 0 },
+      dropColor: "black",
+    };
+  }
 
-  //This state variable keeps track of the current color, which should change on magnetize!
-  const [dropColor,updateDropColor] = useState("blue")
+  setDropZoneRectangle = (event) => {
+    console.log(event.nativeEvent.layout);
+    this.setState({ dropZoneRect: event.nativeEvent.layout }, () => {
+      this.updateDropZonePosition();
+    });
+  };
 
-  return (
-    <View style={styles.container}>
-      
-      <DragObject intialLocation={{top:50,left:100}}/>
+  setDropZoneRadius = (event) => {
+    console.log(event.nativeEvent.layout);
+    this.setState({ dropZoneCircle: event.nativeEvent.layout }, () => {
+      this.updateDropZonePosition();
+    });
+  };
 
-      <View style={styles.lockContainer}>
-        <Text>Drag Here To Magnetize!</Text>
-        <View style={[styles.lockDrop,{borderColor:dropColor}]} />
+  updateDropZonePosition = () => {
+    const { dropZoneRect, dropZoneCircle } = this.state;
+    if (dropZoneRect && dropZoneCircle) {
+      const dropZoneRadius = dropZoneCircle.height;
+      const dropCenterX =
+        dropZoneCircle.width / 2 + dropZoneCircle.x + dropZoneRect.x;
+      const dropCenterY =
+        dropZoneCircle.height / 2 +
+        dropZoneRect.height -
+        dropZoneCircle.height +
+        dropZoneRect.y;
+      this.setState({
+        dropCircle: {
+          x: dropCenterX,
+          y: dropCenterY - 4,
+          radius: dropZoneRadius / 2,
+        },
+      });
+    }
+  };
+
+  boxPositionUpdated = (dragItemBounds) => {
+    const { dropCircle } = this.state;
+    const circleCenterX = dragItemBounds.x + dragItemBounds.radius;
+    const circleCenterY = dragItemBounds.y + dragItemBounds.radius;
+    const itemCircle = {
+      x: circleCenterX,
+      y: circleCenterY,
+      radius: dragItemBounds.radius,
+    };
+    const intersection = this.twoCirclesIntersects(itemCircle, dropCircle);
+    this.setState({ dropColor: intersection ? "blue" : "black" });
+    return {
+      intersects: intersection,
+      positionOfDrop: dropCircle,
+    };
+  };
+
+  twoCirclesIntersects = (circle1, circle2) => {
+    const xd = circle1.x - circle2.x;
+    const yd = circle1.y - circle2.y;
+    const rs = circle1.radius + circle2.radius;
+
+    return xd * xd + yd * yd <= rs * rs;
+  };
+
+  render() {
+    const { dropColor } = this.state;
+    return (
+      <View style={styles.container}>
+        <DragObject
+          intialLocation={{ top: 200, left: 300 }}
+          positioUpdated={this.boxPositionUpdated}
+        />
+
+        <View style={styles.lockContainer} onLayout={this.setDropZoneRectangle}>
+          <Text>Drag Here To Magnetize!</Text>
+          <View
+            style={[styles.lockDrop, { borderColor: dropColor }]}
+            onLayout={this.setDropZoneRadius}
+          />
+        </View>
       </View>
-
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+  safeArea: { backgroundColor: "#ff0000", flex: 1 },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent:"center"
+    backgroundColor: "#ffffff",
   },
   lockContainer: {
     position: "absolute",
-    top: windowHeight * .7
+    top: windowHeight * 0.7,
+    alignSelf: "center",
   },
   lockDrop: {
     height: 120,
@@ -42,5 +113,5 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     borderWidth: 1,
     margin: 5,
-  }
+  },
 });
--- DragObject.js
+++ DragObject.js
@@ -1,25 +1,103 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Animated } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
 
-var windowHeight = Dimensions.get('window').height
-
-
-export default function DragObject(props) {
-
-    return(
-        //Boilerplate style: Implement your draggable component here!
-        <View style={[styles.bubble]}>
-            
-        </View>
+export default class DragObject extends Component {
+  constructor(props) {
+    super(props);
+    const { intialLocation } = this.props;
+    this.translate = new Animated.ValueXY({ x: 0, y: 0 });
+    this.translate.setOffset({
+      x: intialLocation.left,
+      y: intialLocation.top,
+    });
+    this.state = { initX: intialLocation.left, initY: intialLocation.top };
+    this.lastOffset = { x: intialLocation.left, y: intialLocation.top };
+    this._onGestureEvent = Animated.event(
+      [
+        {
+          nativeEvent: {
+            translationX: this.translate.x,
+            translationY: this.translate.y,
+          },
+        },
+      ],
+      { useNativeDriver: false }
     );
+  }
+  _onHandlerStateChange = (event) => {
+    const { initX, initY, bounds } = this.state;
+    const { positioUpdated } = this.props;
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      this.lastOffset.x += event.nativeEvent.translationX;
+      this.lastOffset.y += event.nativeEvent.translationY;
+      const { intersects, positionOfDrop } = positioUpdated({
+        radius: bounds.height / 2,
+        x: this.translate.x.__getValue(),
+        y: this.translate.y.__getValue(),
+      });
+
+      if (intersects) {
+        console.log("intersects");
+        this.lastOffset.x = positionOfDrop.x;
+        this.lastOffset.y = positionOfDrop.y;
+        this.translate.x.setOffset(this.lastOffset.x - bounds.height / 2);
+        this.translate.y.setOffset(this.lastOffset.y - bounds.height / 2);
+        Animated.spring(this.translate, {
+          toValue: { x: 0, y: 0 },
+          friction: 2,
+          useNativeDriver: false,
+        }).start();
+      } else {
+        const { intialLocation } = this.props;
+        this.lastOffset.x = intialLocation.left;
+        this.lastOffset.y = intialLocation.top;
+        this.translate.x.setOffset(this.lastOffset.x);
+        this.translate.y.setOffset(this.lastOffset.y);
+        Animated.spring(this.translate, {
+          toValue: { x: 0, y: 0 },
+          friction: 2,
+          useNativeDriver: false,
+        }).start();
+      }
+    }
+  };
+
+  setBounds = (event) => {
+    this.setState({ bounds: event.nativeEvent.layout });
+    console.log("bounds: ", event.nativeEvent.layout);
+  };
+
+  render() {
+    return (
+      <PanGestureHandler
+        onGestureEvent={this._onGestureEvent}
+        onHandlerStateChange={this._onHandlerStateChange}
+      >
+        <Animated.View
+          style={[
+            styles.bubble,
+            {
+              transform: [
+                { translateX: this.translate.x },
+                { translateY: this.translate.y },
+              ],
+            },
+          ]}
+          onLayout={this.setBounds}
+        />
+      </PanGestureHandler>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    bubble:{
-        position:"absolute",
-        width:70,
-        height:70,
-        borderRadius:35,
-        backgroundColor:"green",
-    }
-})
+  bubble: {
+    position: "absolute",
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: "red",
+    zIndex: 2,
+  },
+});
